879f27de59f2a7efe096650e3187acf89a2f44bc
Aarush Mehta <eighthrush@gmail.com>
2022-02-05 20:27:18 -0500

    driveTrainSubsystem + DriveConstants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,27 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+
+    public static final class DriveConstants {
+        public static final int LEFT_FALCON_1 = 1;
+        public static final int LEFT_FALCON_2 = 2;
+        public static final int RIGHT_FALCON_1 = 3;
+        public static final int RIGHT_FALCON_2 = 4;
+    
+        public static final double WHEEL_DIAMETER = 3.875; //in
+        public static final double WHEEL_RADIUS = WHEEL_DIAMETER / 2.0;
+    
+        public static final double HIGH_GEAR_RATIO = 6.86; //jvn  //5.533243;// fudge values
+        public static final double LOW_GEAR_RATIO = 15.8839779006; //fudge value //9.93;//jvn // fudge values 
+        public static final double DISTANCE_PER_PULSE = (WHEEL_DIAMETER * Math.PI) / 2048; //wheel diam in inches & falcon CPR
+        
+        public static final int[] SHIFTER_SOLENOID = {1,6}; 
+        public static final int GYRO_ID = 13;
+    
+        public static final double MAX_SPEED_INCHES_PER_SEC = 9.08 * 12.0;
+        public static final double MAX_ACCEL_INCHES_PER_SEC2 = 7.5 * 12.0;
+      }
+    
+
+}
